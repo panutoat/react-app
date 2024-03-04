13c50ccda5276fd30ed1c51bcc2129fccb505654
panut <panutoat@gmail.com>
2024-03-04 19:38:26 +0700

    Updates

---
+++ precache-manifest.9bd993be833e26eb0bb4b6502921e058.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "371130ddb34ea8ec8cbbd7452d6c3f1e",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "19b8905c21c20610d853",
+    "url": "/react-app/static/css/main.f8405ac3.chunk.css"
+  },
+  {
+    "revision": "41768f31f927b04398f0",
+    "url": "/react-app/static/js/2.9c179724.chunk.js"
+  },
+  {
+    "revision": "c8c6862818b3fabeea77041199d6aa14",
+    "url": "/react-app/static/js/2.9c179724.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34f4d8cd3053e110cebb",
+    "url": "/react-app/static/js/3.35e6f69c.chunk.js"
+  },
+  {
+    "revision": "19b8905c21c20610d853",
+    "url": "/react-app/static/js/main.26cc835e.chunk.js"
+  },
+  {
+    "revision": "d7ec1553966dd9dedc6d",
+    "url": "/react-app/static/js/runtime-main.0dd1dc66.js"
+  }
+]);
